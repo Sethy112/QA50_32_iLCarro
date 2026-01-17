1f1f0cff917c31a94c32bedc6b2eeb3ec9b19427
Sethy112 <misyus12@gmail.com>
2026-01-17 20:05:00 +0200

    Initial commit

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,13 @@
+package ui_tests;
+
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class LoginTests extends AppManager {
+    @Test
+    public  void loginPositiveTest(){
+        HomePage homePage= new HomePage(getDriver());
+        homePage.clickBtnLogin();
+    }
+}
